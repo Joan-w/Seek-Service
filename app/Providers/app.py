07dc3643ec0c7bd2e94997f4c453c7f00fcf16ff
joan-w <wamuyujoan4@gmail.com>
2020-05-14 18:47:48 +0300

    Validating the registration form

--- app/Providers/app.py
+++ app/Providers/app.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, render_template, redirect
 from forms import RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '6d6f17faad7ed099602b06c4aa7837dc'
 
 
 providers = [
@@ -32,10 +33,20 @@ providers = [
 def service_providers():
     return render_template('providers.html', title = "Service Providers", providers=providers)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title='Register Service Provider', form=form)
+    if form.validate_on_submit():
+        # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # provider = Provider(first_name=form.first_name.data, last_name=form.last_name.data, 
+        #                     username=form.username.data, email=form.email.data, 
+        #                     phone_number=form.phone_number.data, county=form.county.data, 
+        #                     specialty=form.specialty.data, bio=form.bio.data, password=hashed_password)
+        # db.session.add(provider)
+        # db.session.commit()
+        flash('Your account has been successfully created! You are now available for booking services.', 'success')
+        return redirect(url_for('service_providers'))
+    return render_template('register.html', title = "Register", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
