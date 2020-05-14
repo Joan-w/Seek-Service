8f1caa930487a4b243618a15d5411c09201ac1cb
joan-w <wamuyujoan4@gmail.com>
2020-05-14 21:31:24 +0300

    Adding all te routes

---
+++ app/Providers/providers/routes.py
@@ -0,0 +1,45 @@
+from flask import url_for, render_template, redirect, flash
+from providers import app
+from providers.forms import RegistrationForm
+from providers.models import Provider
+
+providers = [
+    {
+        'full_name' : 'Joan Kinyua',
+        'username' : 'Joan',
+        'email' : 'muyu@gmail.com',
+        'phone_number' : '123 456 789',
+        'county' : 'Nairobi',
+        'specialty' : 'Interior designer',
+        'bio' : 'I was born to be a designer. My designing techniques are all you need'
+    },
+    {
+        'first_name' : 'Simon Mathu',
+        'username' : 'Simon',
+        'email' : 'mathu@gmail.com',
+        'phone_number' : '023 456 789',
+        'county' : 'Kitui',
+        'specialty' : 'Plumber',
+        'bio' : 'Fixing people\'s taps and make the happy makes me more happier.'
+    }
+]
+
+
+@app.route('/providers')
+def service_providers():
+    return render_template('providers.html', title = "Service Providers", providers=providers)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # provider = Provider(first_name=form.first_name.data, last_name=form.last_name.data, 
+        #                     username=form.username.data, email=form.email.data, 
+        #                     phone_number=form.phone_number.data, county=form.county.data, 
+        #                     specialty=form.specialty.data, bio=form.bio.data, password=hashed_password)
+        # db.session.add(provider)
+        # db.session.commit()
+        flash('Your account has been successfully created! You are now available for booking services.', 'success')
+        return redirect(url_for('service_providers'))
+    return render_template('register.html', title = "Register", form=form)
