29ff3be39e432d6f60366c7b2c3c9fd4648fddc5
joan-w <wamuyujoan4@gmail.com>
2020-05-14 17:35:38 +0300

    Testing if the app is running

---
+++ app/Providers/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/register')
+def register():
+    title = '<h1>Boss Bitch</h1>'
+    return title
+
+if __name__ == '__main__':
+    app.run(debug=True)
