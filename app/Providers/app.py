91c8db2b15f6baa9b6415eb2d2372e77e4b92cd9
joan-w <wamuyujoan4@gmail.com>
2020-05-14 17:43:54 +0300

    Adding routes one for the provoders and for the registration of the providers

--- app/Providers/app.py
+++ app/Providers/app.py
@@ -3,6 +3,30 @@ from flask import Flask
 app = Flask(__name__)
 
 
+providers = [
+    {
+        'first_name' : 'Joan',
+        'last_name' : 'Kinyua',
+        'username' : 'Joan',
+        'county' : 'Nairobi',
+        'specialty' : 'Interior designer',
+        'bio' : 'I was born to be a designer. My designing techniques are all you need'
+    },
+    {
+        'first_name' : 'Simon',
+        'last_name' : 'Mathu',
+        'username' : 'Simon',
+        'county' : 'Kitui',
+        'specialty' : 'Plumber',
+        'bio' : 'Fixing people\'s taps and make the happy makes me more happier.'
+    }
+]
+
+
+@app.route('/providers')
+def service_providers():
+    return render_template('providers.html', title = "Service Providers", providers=providers)
+
 @app.route('/register')
 def register():
     title = '<h1>Boss Bitch</h1>'
