3e65cbbb3214b5f05c76fb6db5cb7fb11d0410df
joan-w <wamuyujoan4@gmail.com>
2020-05-14 17:47:08 +0300

    Adding dummy data

--- app/Providers/app.py
+++ app/Providers/app.py
@@ -8,6 +8,8 @@ providers = [
         'first_name' : 'Joan',
         'last_name' : 'Kinyua',
         'username' : 'Joan',
+        'email' : 'muyu@gmail.com',
+        'phone_number' : '123 456 789',
         'county' : 'Nairobi',
         'specialty' : 'Interior designer',
         'bio' : 'I was born to be a designer. My designing techniques are all you need'
@@ -16,6 +18,8 @@ providers = [
         'first_name' : 'Simon',
         'last_name' : 'Mathu',
         'username' : 'Simon',
+        'email' : 'mathu@gmail.com',
+        'phone_number' : '023 456 789',
         'county' : 'Kitui',
         'specialty' : 'Plumber',
         'bio' : 'Fixing people\'s taps and make the happy makes me more happier.'
